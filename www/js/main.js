9754e9d981a1b1315959edc604d27d99281d5f1a
Ayush <vegasrocks619@gmail.com>
2015-08-16 13:41:49 +0530

    integration part 1

---
+++ www/js/main.js
@@ -0,0 +1,19 @@
+
+$("#sliderContainer").vTiwari({
+	// dislike callback
+    onDislike: function (item) {
+	    // Perform some logic Gaurav, Gaurav and Ayush for you :P
+       
+    },
+	// like callback
+    onLike: function (item) {
+	    // Perform some logic Gaurav, Gaurav and Ayush for you :P
+        
+    },
+	animationRevertSpeed: 200,
+	animationSpeed: 400,
+	threshold: 1,
+	likeSelector: '.like',
+	dislikeSelector: '.dislike'
+});
+
