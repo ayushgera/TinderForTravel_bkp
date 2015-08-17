d3c8036baf4b4fe941d9f9b4bd28a1eb08ec5345
Ayush <vegasrocks619@gmail.com>
2015-08-18 01:47:24 +0530

    google maps issue fixed

--- www/js/places.js
+++ www/js/places.js
@@ -64,9 +64,8 @@ function onSuccess(position) {
 
  	map.fitBounds(bounds);
 
-	map.setCenter(bounds.getCenter(), 
-	                    map.getBoundsZoomLevel(bounds));
-					
+	//map.setCenter(bounds.getCenter(), map.getBoundsZoomLevel(bounds));
+	getZoomByBounds(map,bounds);			
 	google.maps.event.addListener(marker1, 'mousedown', function(){
 		slider.goToSlide(0)	;
 	});				
@@ -83,6 +82,53 @@ function onSuccess(position) {
 		slider.goToSlide(4)	
 	});
 }
+
+/**
+* Returns the zoom level at which the given rectangular region fits in the map view. 
+* The zoom level is computed for the currently selected map type. 
+* @param {google.maps.Map} map
+* @param {google.maps.LatLngBounds} bounds 
+* @return {Number} zoom level
+**/
+
+function MProjection() {
+}
+MProjection.prototype.fromLatLngToPoint = function(latlng) {
+    var x = (latlng.lng() + 180) / 360 * 256;
+    var y = ((1 - Math.log(Math.tan(latlng.lat() * Math.PI / 180) + 1 / Math.cos(latlng.lat() * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, 0)) * 256;
+    return new google.maps.Point(x, y);
+};
+
+function getZoomByBounds( map, bounds ){
+  var MAX_ZOOM =  map.getMapTypeId().maxZoom || 21 ;
+  var MIN_ZOOM =  map.getMapTypeId().minZoom || 0 ;
+
+	
+	// Wait for idle map
+	//google.maps.event.addListener(map, 'idle', function() {
+	   // Get projection
+	  // projection = overlay.getProjection();
+	   
+	     var ne= new MProjection().fromLatLngToPoint( bounds.getNorthEast() );
+	  var sw= new MProjection().fromLatLngToPoint( bounds.getSouthWest() ); 
+
+	  var worldCoordWidth = Math.abs(ne.x-sw.x);
+	  var worldCoordHeight = Math.abs(ne.y-sw.y);
+
+	  //Fit padding in pixels 
+	  var FIT_PAD = 40;
+
+	  for( var zoom = MAX_ZOOM; zoom >= MIN_ZOOM; --zoom ){ 
+		  if( worldCoordWidth*(1<<zoom)+2*FIT_PAD < $(map.getDiv()).width() && 
+			  worldCoordHeight*(1<<zoom)+2*FIT_PAD < $(map.getDiv()).height() )
+			  return zoom;
+	  }
+	   
+	//})
+
+  return 0;
+}
+
 function onError(error) {
 	alert('code: ' + error.code + '\n' +
 	'message: ' + error.message + '\n');
